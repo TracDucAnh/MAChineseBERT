83c9d97af3183892732aa7785421cd8cc7918c6d
Đinh Trác Đức Anh <dinhtracducanh@MacBook-Air-cua-inh.local>
2025-12-15 18:09:48 +0700

    add model

--- __init__.py
+++ __init__.py
@@ -1,11 +1,15 @@
 from .tokenizer import MorphemeAwareTokenizer
 from .embeddings import BoundaryAwareEmbeddings
-from .model import MorphemeAwareRERTModel
+from .model import MorphemeAwareBertModel
+from .model import MorphemeAwareBertForMaskedLM
+from .model import MorphemeAwareBertForSequenceClassification
 from .bias_utils import create_bias_matrix
 
 __all__ = [
     "MorphemeAwareTokenizer",
     "BoundaryAwareEmbeddings",
     "MorphemeAwareBERTModel",
+    "MorphemeAwareBertForMaskedLM",
+    "MorphemeAwareBertForSequenceClassification",
     "create_bias_matrix",
 ]
